6adf7cd45623ba0c6bccc740c9b71f31685098c6
SamDC <sam.davis.castro@gmail.com>
2019-01-24 04:06:23 -0500

    unsure what is preventing expected output...no compile or linter errors. console is saying exports is not defined

--- Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
+++ Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
@@ -1,9 +1,7 @@
-/// <reference path="../../../typings/require.d.ts" />
-/// <reference path="../../../../../../../node_modules/@types/requirejs/index.d.ts" />
+/// <reference path="/Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/typings/require.d.ts" />
 require.config({
     baseUrl: 'scripts/amd-demo'
 });
 require(['bootstrapper'], function (bootstrapper) {
     bootstrapper.run();
 });
-// /Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
--- Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.ts
+++ Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.ts
@@ -1,9 +1,8 @@
-/// <reference path="../../../typings/require.d.ts" />
+/// <reference path="/Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/typings/require.d.ts" />
 
 require.config({
     baseUrl: 'scripts/amd-demo'
-})
+});
 require(['bootstrapper'], function (bootstrapper) {
     bootstrapper.run();
-})
-// /Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
+});
