6f9bdaa0751cba317f6b42c4187c44d39b051b3f
SamDC <sam.davis.castro@gmail.com>
2019-01-24 00:31:34 -0500

    init commit

---
+++ Before/TypeScriptModules/TypeScriptModules/Scripts/app/04-02-internal-module.ts
@@ -0,0 +1,23 @@
+/// <reference path="/Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/toastr.d.ts" />
+
+interface IRectangle {
+    height: number;
+    width: number;
+    getArea(): number;
+}
+namespace Shapes {
+    export class Rectangle implements IRectangle {
+        constructor(public height: number, public width: number) {
+        }
+        getArea() { return this.height * this.width };
+    }
+}
+
+namespace MyProgram {
+    function run () {
+        var rect : IRectangle = new Shapes.Rectangle(10,4);
+        var area = rect.getArea();
+        toastr.info(`Area = ${area}`);
+    }
+    run();
+}
