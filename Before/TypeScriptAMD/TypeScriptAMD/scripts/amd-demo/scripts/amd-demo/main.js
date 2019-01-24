2f43a6526dfc5eed5c5dacce694e86f1ca1f202b
SamDC <sam.davis.castro@gmail.com>
2019-01-24 02:43:25 -0500

    accidentally added node modules...removing

--- Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
+++ Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
@@ -1,9 +1,9 @@
 /// <reference path="../../../typings/require.d.ts" />
+/// <reference path="../../../../../../../node_modules/@types/requirejs/index.d.ts" />
 require.config({
-    baseUrl: 'Before/TypeScriptAMD/TypeScriptAMD'
+    baseUrl: 'scripts/amd-demo'
 });
 require(['bootstrapper'], function (bootstrapper) {
     bootstrapper.run();
 });
 // /Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
-// let // / <reference path="../../../../../../../node_modules/@types/requirejs/index.d.ts" />
--- Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.ts
+++ Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.ts
@@ -1,10 +1,9 @@
 /// <reference path="../../../typings/require.d.ts" />
 
 require.config({
-    baseUrl: 'Before/TypeScriptAMD/TypeScriptAMD'
+    baseUrl: 'scripts/amd-demo'
 })
-require(['bootstrapper'], bootstrapper => {
+require(['bootstrapper'], function (bootstrapper) {
     bootstrapper.run();
 })
 // /Users/samdc/Desktop/psTypescriptFundamentals/typeScriptTest3/Before/TypeScriptAMD/TypeScriptAMD/scripts/amd-demo/scripts/amd-demo/main.js
-// let // / <reference path="../../../../../../../node_modules/@types/requirejs/index.d.ts" />
